af165b5dd9b46aaffcc08be6b5169974e179d164
guddytech <guddytechs@gmail.com>
2023-05-01 17:44:50 +0100

    added some variables

--- tutorial.go
+++ tutorial.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("This is GuddyTech");
+	var number uint16 = 260;
+	number = number + 10;
+	var detail = "I am a " + "number" ;
+	fmt.Println("This is GuddyTech", number, detail);
 }
